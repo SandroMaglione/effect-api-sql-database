06c992ca62496d4c664f8b76a78e10f12c6510e7
SandroMaglione <lass.maglio@gmail.com>
2025-02-15 07:09:39 +0100

    select api endpoint

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,4 +1,9 @@
-import { HttpApi, HttpApiEndpoint, HttpApiGroup } from "@effect/platform";
+import {
+  HttpApi,
+  HttpApiEndpoint,
+  HttpApiGroup,
+  HttpApiSchema,
+} from "@effect/platform";
 import { Schema } from "effect";
 
 export class User extends Schema.Class<User>("User")({
@@ -7,11 +12,19 @@ export class User extends Schema.Class<User>("User")({
   created_at: Schema.DateFromSelf,
 }) {}
 
-class RestGroup extends HttpApiGroup.make("rest").add(
-  HttpApiEndpoint.post("create-user")`/user/create`
-    .setPayload(Schema.Struct({ name: Schema.String }))
-    .addError(Schema.String)
-    .addSuccess(User)
-) {}
+class RestGroup extends HttpApiGroup.make("rest")
+  .add(
+    HttpApiEndpoint.post("create-user")`/user/create`
+      .setPayload(Schema.Struct({ name: Schema.String }))
+      .addError(Schema.String)
+      .addSuccess(User)
+  )
+  .add(
+    HttpApiEndpoint.get(
+      "get-user"
+    )`/user/get/${HttpApiSchema.param("id", Schema.NumberFromString)}`
+      .addError(Schema.String)
+      .addSuccess(User)
+  ) {}
 
 export class ServerApi extends HttpApi.make("server-api").add(RestGroup) {}
